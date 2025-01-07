84dc023d1f1ce6e27cb941f8f6938530e837836e
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-01-07 12:08:35 +0530

    myfirst commit

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Calculator{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers:");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println("Enter operation:");
+		char sign = sc.next().charAt(0);
+		double res;
+		switch(sign){
+		    case '+':
+			res = num1+num2;
+			System.out.println(res);
+			break;
+		    default:
+			System.out.println("invalid operation");
+
+		}
+		
+	}
+}
